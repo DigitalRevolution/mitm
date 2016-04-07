9b1f4caf583f519d2ebfa0be674dde9976fab924
Digital Revolution <stephen.vandelinder@gmail.com>
2016-04-07 17:28:53 -0600

    Organized file system, tested updates, added gitignore, removed lookup.html

---
+++ js/makemap.js
@@ -0,0 +1,40 @@
+'use strict';
+var map;
+      var infowindow;
+
+      function initMap() {
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: centerPoint,
+          zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: centerPoint,
+          radius: 1000,
+          type: ['food']
+        }, callback);
+      }
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
